478eb17bc063edd0eccf9bd635bdee01cb45b031
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-02-11 10:56:43 +0100

    First import

---
+++ install.js
@@ -0,0 +1,60 @@
+'use strict'
+
+/*
+ * This installs a Makefile into any project that has fakefile as a dependency.
+ * We'll overwrite known Fakefiles, but no unknown Makefiles via sha1 matchting.
+ *
+ * No babel ES6 transpilation or other hipster goodies in here,
+ * regard this as a simple procedural bash script, that's more portable : )
+ *
+ * The meat of this project can be found in ./Makefile
+ */
+
+var fs = require('fs-extra')
+var path = require('path')
+var crypto = require('crypto')
+var rootDir = path.normalize(__dirname + '/../..')
+var dstPackagePath = rootDir + '/package.json'
+var dstPackage = {}
+var dstMakePath = rootDir + '/Makefile'
+var dstMakeBody = ''
+var dstMakeSha = ''
+var srcMakePath = path.normalize(__dirname + '/Makefile')
+var srcMakeBody = fs.readFileSync(srcMakePath)
+var srcMakeSha = crypto.createHash('sha1').update(srcMakeBody).digest('hex')
+var knownShas = [
+  ''
+]
+
+try {
+  dstMakeBody = fs.readFileSync(dstMakePath)
+  dstMakeSha = crypto.createHash('sha1').update(dstMakeBody).digest('hex')
+} catch (e) {
+  dstMakeBody = false
+}
+
+try {
+  dstPackage = require(dstPackagePath)
+}catch (e) {
+  dstPackage = false
+}
+
+if (!dstPackage.dependencies) {
+  console.error('No valid package.json found at ' + dstPackagePath + '. Skipping. ')
+  process.exit(0)
+}
+
+if (!dstMakeBody || knownShas.indexOf(dstMakeSha) > -1) {
+  if (srcMakeSha === dstMakeSha) {
+    console.error('Already on the right Fakefile. Skipping. ')
+    process.exit(0)
+  }
+
+  console.error('No or known Makefile found at ' + srcMakePath + ' installing ours to ' + dstMakePath + ' ')
+  fs.copySync(srcMakePath, dstMakePath, {clobber: true})
+  process.exit(0)
+}
+
+console.error('I found a Makefile at ' +  dstMakePath +' that I do not know. (sha1: ' + dstMakeSha + ')')
+console.error('I will not risk overwritting it. ')
+console.error('Remove it first manually and install Fakefile again. ')
